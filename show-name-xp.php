4c6f39380ba7a4e9a6bc70c73fe05ca5ff14c50b
dovudwkt <dinomov99@gmail.com>
2019-12-04 21:02:57 +0200

    now the script shows not only name but also loses and wins

--- show-name-xp.php
+++ show-name-xp.php
@@ -3,11 +3,11 @@ session_start();
 $_SESSION['current_user'];
  // $dbcon = new mysqli('localhost', 'root', '', 'game');
 include("mysqli_connect.php");  	
-  $query = "SELECT username, credits FROM users where id =". $_SESSION['current_user'];
+  $query = "SELECT username, credits, wins, loses FROM users where id =". $_SESSION['current_user'];
 	$result = mysqli_query($dbcon, $query);
 	if($result) { 
 		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-			echo "<p>".$row['username']." | ". $row['credits']. "xp</p>";
+			echo "<p>".$row['username']." | ". $row['credits']. "xp | <span style='color:green' title='wins'>".$row['wins']."</span> | <span style='color:red' title='loses'>".$row['loses']."</span> </p>";
 		}
 	}
 
