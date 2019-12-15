05bbc9b9ec56ee68f45a465ec24108859dc26be2
dovudwkt <dinomov99@gmail.com>
2019-12-15 20:33:46 +0200

    lab 10

--- labs/lab10/login.php
+++ labs/lab10/login.php
@@ -7,52 +7,73 @@
 
 <?php 
 if(isset($_POST['login'])){
-extract($_POST);
+// extract($_POST);
+$username = $_POST['username'];
+$passwrd = $_POST['passwrd'];
 
 $conn = mysqli_connect("localhost", "root", "", "autos");
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+// ---------------Procedural Method----------------------
+
+ $stmt = mysqli_stmt_init($conn);
+$sql = "SELECT username, passwrd FROM users where username = ?";
+ mysqli_stmt_prepare($stmt, $sql);
+ mysqli_stmt_bind_param($stmt, "s", $username);
+ mysqli_stmt_execute($stmt);
+ $result = mysqli_stmt_get_result($stmt);
+ $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+ if (mysqli_num_rows($result) == 1) {
+   if($passwrd == $row['passwrd']){ 
+     session_start();
+     $_SESSION['user'] = 'logged';
+     $url = "menu.php";   
+     header('Location: ' . $url);
+   }
+ }
+
 
 
 // ---------------OOP Method----------------------
-$stmt = $conn->stmt_init();
-$sql = "SELECT passwrd FROM users where username = ?";
+// $stmt = $conn->stmt_init();
+// $sql = "SELECT passwrd FROM users where username = ?";
 
-$stmt->prepare($sql);
-$stmt->bind_param('s', $username);
-$stmt->execute() or die("Query error: ");
-$stmt->store_result();
-$stmt->bind_result($pswd);
+// $stmt->prepare($sql);
+// $stmt->bind_param('s', $username);
+// $stmt->execute() or die("Query error: ");
+// $stmt->store_result();
+// $stmt->bind_result($pswd);
 
-if ($stmt->num_rows>0)
-{
-  $stmt->fetch();
-  if($passwrd == $pswd){ 
-      session_start();
-      $_SESSION['user'] = 'logged';
-      $url = "menu.php";   
-      header('Location: ' . $url);
-  }  
-}
-// ---------------Procedural Method----------------------
+// if ($stmt->num_rows>0)
+// {
+//   $stmt->fetch();
+//   if($passwrd == $pswd){ 
+//       session_start();
+//       $_SESSION['user'] = 'logged';
+//       $url = "menu.php";   
+//       header('Location: ' . $url);
+//   }  
+// }
 
-// $sql = "SELECT username, passwrd FROM users where username = ?";
-//  $q = mysqli_stmt_init($conn);
-//  mysqli_stmt_prepare($q, $sql);
-//  mysqli_stmt_bind_param($q, "s", $username);
-//  mysqli_stmt_execute($q);
-//  $result = mysqli_stmt_get_result($q);
-//  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-//  if (mysqli_num_rows($result) == 1) {
-//    if($passwrd == $row['passwrd']){ 
-//      session_start();
-//      $_SESSION['user'] = 'logged';
-//      $url = "menu.php";   
-//      header('Location: ' . $url);
-//    }
-//  }
+
+//-----------Primtitve vesrion------------
+// $stmt = mysqli_stmt_init($conn);
+// $sql = "SELECT * FROM users where username = ? AND passwrd = ?";
+// mysqli_stmt_prepare($stmt, $sql);
+// mysqli_stmt_bind_param($stmt, 'ss', $username, $passwrd);
+// mysqli_stmt_execute($stmt);
+// $result = mysqli_stmt_get_result($stmt);
+// $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+// if(mysqli_num_rows($result)>0){
+//     session_start();
+//     $_SESSION['user'] = 'logged';
+//     $url = "menu.php";   
+//     header('Location: ' . $url);
+// }else{
+//   echo "failed! Password or username is incorrect";
+// }
+
 
 
 }
